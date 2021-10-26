1833148035b6c46d218fa91aac4d29de3967b8e7
rdogm13 <drg6525@yahoo.com>
2021-10-26 00:49:00 -0700

    brand colors / hover animation on hero banner mouseover

--- js/hero-swiper.js
+++ js/hero-swiper.js
@@ -1,18 +1,54 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 3,
-    spaceBetween: 0,
-    loop: true,
-    lazy: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  lazy: true,
 
-  });
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // mobile breakpoints for number of slides displayed
+
+  breakpoints: {
+    820: {
+      slidesPerView: 2,
+    },
+    1400: {
+      slidesPerView: 3,
+    },
+  },
+
+});
+
+
+// $(document).ready(function () {
+
+//   $('.swiper-slide').mouseenter(function () {
+//     $(this).addClass('focused');
+//   })
+
+//   $('.swiper-slide').mouseleave(function () {
+//     $(this).removeClass('focused');
+//   })
+
+// })
+
+
+$(document).on({
+  mouseenter: function () {
+    $(this).addClass('focused');
+
+  },
+  mouseleave: function () {
+    $(this).removeClass('focused');
+
+  }
+}, '.swiper-slide')
